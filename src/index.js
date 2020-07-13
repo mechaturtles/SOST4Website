8f29ae2796332a8aadb072d04612cbe183209fec
Bryce Villanueva <bryce.christian@gmail.com>
2020-07-13 01:31:51 -0500

    Implemented .env and sign up
    A .env file is required to run the project, and the signup page runs correctly.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from './components/App';
 
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+//Using Strict Mode
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
