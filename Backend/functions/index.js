24326e2e6b158ec7fa723f2f32232b38cdce81d2
Bryce Villanueva <bryce.christian@gmail.com>
2020-07-14 15:56:34 -0500

    Cloud Functions initial setup

---
+++ Backend/functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// In a real-world implementation, requests to third-party APIs would be done here. It is not possible to make outbound network requests using Firebase's Free Plan, so a workaround is done here so that the client processes the request. 
+// This is, however, a terrible idea in real production but is only done to demonstrate our ability to interface with third-party devices and is by no means a permanent solution.
+
+ 
