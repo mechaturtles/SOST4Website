b48cf3593b4dd6e481e8334b034d6aa1717dfdd0
Dan Chepkwony <dan.k.chepkwony@gmail.com>
2020-07-17 15:31:15 -0400

    Base styling

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -20,7 +20,7 @@ const Navigation = () => (
 //Admin should be hidden for non-admin roles
 const NavigationAuth = () => (
   <div className = "Top-bar"> 
-          <div className = "Top-link-logo"><p className = "Top-logo"> <Link to={ROUTES.HOME} className = "Top-no-decoration"> MyHealthRadar </Link> </p></div>
+          <div className = "Top-link-logo"><p className = "Top-logo"> <Link to={ROUTES.HOME} className = "Top-no-decoration"> myHealthRadar </Link> </p></div>
           <div className = "Top-link-middle"><p className = "Top-navigation "> <Link to={ROUTES.HOME} className = "Top-no-decoration"> Appointments </Link> </p></div>
           <div className = "Top-link-middle"><p className = "Top-navigation Top-more-bottom"> <Link to={ROUTES.HOME} className = "Top-no-decoration"> Contact Provider </Link> </p></div>
           <div className = "Top-link-middle"><p className = "Top-navigation Top-more-bottom"> <Link to={ROUTES.HOME} className = "Top-no-decoration"> Past Data </Link> </p></div>
@@ -31,7 +31,7 @@ const NavigationAuth = () => (
 
 const NavigationNonAuth = () => (
   <div className = "Top-bar"> 
-           <div className = "Top-link-logo"><p className = "Top-logo"> <Link to={ROUTES.LANDING} className = "Top-no-decoration"> MyHealthRadar </Link> </p></div>
+           <div className = "Top-link-logo"><p className = "Top-logo"> <Link to={ROUTES.LANDING} className = "Top-no-decoration"> myHealthRadar </Link> </p></div>
           <div className = "Top-link-sign-in"><p className = "Top-navigation"> <Link to={ROUTES.SIGN_IN} className = "Top-no-decoration "> Sign In </Link> </p></div>
   </div>
 );
